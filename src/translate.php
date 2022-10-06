2924a2a5dd3bf44273f32cb4f97db1cccb1a76bf
Neider Ruiz <57574910+neiderruiz@users.noreply.github.com>
2022-10-06 15:20:51 -0500

    base translations

---
+++ src/translate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace neiderruiz\translatorphp;
+
+class Translate
+{
+    public function translate(string $message, string $destination, string $entryLanguage = 'en'): string
+    {
+        $translateBase = new BaseTranslate();
+
+        return $translateBase->translate($message, $destination, $entryLanguage);
+    }
+}
